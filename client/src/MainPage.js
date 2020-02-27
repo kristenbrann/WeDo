a1aacd39e1e6d5f28b1b3c7fac78ab9939bf06f4
Chris Paika <paika.christopher@gmail.com>
2020-02-26 21:56:05 -0500

    Super basic input for tasks

--- client/src/MainPage.js
+++ client/src/MainPage.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
-function MainPage() {
-  return (
-    <h1>Welcome to your todo list!</h1>
-  );
+class MainPage extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log('Adding a task: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  
+  render() {
+    return (
+      <div>
+        <h1>Welcome to your todo list!</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input type="submit" value="Add a WeDo" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default MainPage;
