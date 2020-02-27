5b72635157e147297f81fac77dc074d11ddc436c
Chris Paika <paika.christopher@gmail.com>
2020-02-26 20:58:45 -0500

    Working redirects

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams,
+  Redirect
+} 
+from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename = "/">
+      <Switch>
+        <Route exact path="/">
+          <About />
+        </Route>
+        <Route path="/list/:id" >
+          <MainPage/>
+        </Route>
+      </Switch>
+    </Router>
   );
+
+  function About() {
+    var redirectURL = "/list/" + "12345678"
+    return (
+      <Redirect to={redirectURL}/>
+    )
+  }
+
+  function MainPage() {
+    let { id } = useParams();
+    return (
+      <h1>{id}</h1>
+    )
+  }
 }
 
 export default App;
