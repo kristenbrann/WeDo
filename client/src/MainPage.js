5ae280b2e63c00ea006bec113e9fb65b23c3c8e7
kristenbrann <tellkristenstuff@gmail.com>
2020-03-26 21:24:39 -0400

    Update MainPage to be interactive to do list (#12)
    Co-authored-by: Brann <kbrann@constantcontact.com>

--- client/src/MainPage.js
+++ client/src/MainPage.js
@@ -1,36 +1,76 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import './MainPage.css';
 
-class MainPage extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
+const defaultTasks = [
+  {
+    description: 'Water the plants',
+  },
+  {
+    description: 'Water the plants',
+  },
+  {
+    description: 'Water the plants',
+  },
+  {
+    description: 'Water the plants',
+  },
+];
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+const MainPage = () => {
+  const [tasks, setTasks] = useState(defaultTasks);
+  const [newTask, setNewTask] = useState('');
+  const inputEl = useRef(null);
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
+  /**
+   * Managing the value of the new task input
+   * @param e
+   */
+  const onChangeNewTask = (e) => {
+    e.preventDefault();
+    setNewTask(e.target.value);
+  };
 
-  handleSubmit(event) {
-    console.log('Adding a task: ' + this.state.value);
-    event.preventDefault();
-  }
+  /**
+   * Listens for "enter" presses so submit the new task input
+   * @param e
+   */
+  const onKeyPressNewTask = (e) => {
+    if (e.charCode === 13 || e.keyCode === 13 || e.key === 'Enter') {
+      e.preventDefault();
+      e.stopPropagation();
+      onAddNewTask(e);
+    }
+  };
 
-  
-  render() {
-    return (
+  /**
+   * Adds a new task to the task list. Re-focuses on input so user can keep adding.
+   * @param e
+   */
+  const onAddNewTask = (e) => {
+    e.preventDefault();
+    const newTasks = tasks.concat([{
+      description: newTask,
+    }]);
+    setTasks(newTasks);
+    setNewTask('');
+    inputEl.current.focus();
+  };
+
+  return (
       <div>
         <h1>Welcome to your todo list!</h1>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-          <input id="addTask" type="submit" value="Add a WeDo" />
-        </form>
+        {tasks.map(t => (
+            <div className="task">
+              <p className="title">{t.description}</p>
+              <button className="complete">&#10004;</button>
+            </div>
+        ))}
+        <div className="task">
+        <input type="text" ref={inputEl} value={newTask} onKeyPress={onKeyPressNewTask} onChange={onChangeNewTask}/>
+        <button onClick={onAddNewTask}>&#43;</button>
+        </div>
       </div>
-    );
-  }
-}
+  );
+};
 
 export default MainPage;
