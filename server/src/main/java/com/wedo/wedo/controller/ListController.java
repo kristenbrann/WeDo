ab239554b68d22b20491902d41b10872c2342f09
Chris Paika <paika.christopher@gmail.com>
2020-03-26 21:04:07 -0400

    Basic List posting

--- server/src/main/java/com/wedo/wedo/controller/ListController.java
+++ server/src/main/java/com/wedo/wedo/controller/ListController.java
@@ -1,15 +1,16 @@
 package com.wedo.wedo.controller;
 
+import com.wedo.wedo.dto.ListDto;
 import com.wedo.wedo.dto.TaskDto;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TaskController {
+public class ListController {
 
-    @PostMapping("/v1/tasks")
-    TaskDto newTask(@RequestBody TaskDto taskDto) {
-        return taskDto;
+    @PostMapping("/v1/lists")
+    ListDto newList(@RequestBody ListDto listDto) {
+        return listDto;
     }
 }
---
+++ server/src/main/java/com/wedo/wedo/dto/ListDto.java
@@ -0,0 +1,52 @@
+package com.wedo.wedo.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Objects;
+
+public class ListDto {
+
+    @JsonProperty("list_id")
+    private String listId;
+
+    @JsonProperty("list_name")
+    private String listName;
+
+    public String getListId() {
+        return listId;
+    }
+
+    public void setListId(String listId) {
+        this.listId = listId;
+    }
+
+    public String getListName() {
+        return listName;
+    }
+
+    public void setListName(String listName) {
+        this.listName = listName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ListDto listDto = (ListDto) o;
+        return Objects.equals(listId, listDto.listId) &&
+                Objects.equals(listName, listDto.listName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(listId, listName);
+    }
+
+    @Override
+    public String toString() {
+        return "ListDto{" +
+                "listId='" + listId + '\'' +
+                ", listName='" + listName + '\'' +
+                '}';
+    }
+}
