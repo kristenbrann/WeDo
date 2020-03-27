a42e0ca952c8e9a05f0fc1e06bdf674b0376a09f
Brann <kbrann@constantcontact.com>
2020-03-26 21:14:18 -0400

    Update MainPage to be interactive to do list

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,21 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  useParams,
   Redirect
 } from "react-router-dom";
 import './App.css';
 import MainPage from './MainPage.js';
 import shortid from "shortid";
 
-function App() {
+const CreateList = () => {
+  const id = shortid.generate();
+  const redirectURL = `/list/${id}`;
   return (
+      <Redirect to={redirectURL}/>
+  );
+};
+
+const App = () => (
     <Router basename = "/">
       <Switch>
         <Route exact path="/">
@@ -22,15 +28,6 @@ function App() {
         </Route>
       </Switch>
     </Router>
-  );
-
-  function CreateList() {
-    var id = shortid.generate();
-    var redirectURL = "/list/" + id;
-    return (
-      <Redirect to={redirectURL}/>
-    )
-  }
-}
+);
 
 export default App;
