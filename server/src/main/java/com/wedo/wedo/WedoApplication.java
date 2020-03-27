aa223d8346872fc8cc61875d66f1e9090a6b87e6
Chris Paika <paika.christopher@gmail.com>
2020-03-26 20:31:38 -0400

    Initializing Spring Boot backend

---
+++ server/src/main/java/com/wedo/wedo/WedoApplication.java
@@ -0,0 +1,13 @@
+package com.wedo.wedo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WedoApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/wedo/wedo/WedoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedo.wedo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WedoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
